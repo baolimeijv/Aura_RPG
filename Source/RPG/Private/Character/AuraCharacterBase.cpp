a0cddf9efb2f543e5f7ebe68f69871d603050f6f
baolimeijv <2014372542@qq.com>
2026-09-14 19:52:25 +0800

    初始化仓库，aura和goblin蓝图和默认动画创建

---
+++ Source/RPG/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
